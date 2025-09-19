a20bd6a623944b843e89b2e974eca22205f15068
melina <kouprojects08@gmail.com>
2025-09-19 21:43:49 +0700

    versi terbaru

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({super.key}); // ✅ pakai super.key
+  const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('About Page')),
-      body: const Center(child: Text('Ini halaman About')),
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text(
+          'Aplikasi ini dibuat untuk menampilkan daftar alat digital art,\n'
+          'dilengkapi dengan fitur counter sederhana,\n'
+          'serta informasi About & Contact.',
+          style: TextStyle(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
